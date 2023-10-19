7d1e5e1589a3201eb6aad89499b11004c04adf7b
sbx_user1051 <sbx_user1051@169.254.25.165>
2023-10-19 21:22:55 +0000

    Thu Oct 19 21:22:55 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,35 @@
+"""
 
+Our price table and offers:
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+
+
+Notes:
+ - For any illegal input return -1
+
+In order to complete the round you need to implement the following method:
+     checkout(String) -> Integer
+
+Where:
+ - param[0] = a String containing the SKUs of all the products in the basket
+ - @return = an Integer representing the total checkout value of the items
+"""
+
+from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    item_counts = Counter(skus)
+    for k,v in item_counts.items():
+        print(k,v)
+
+checkout("AABCE")
+
