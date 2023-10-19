b5db23de7f4c033aef551a344bf19ae53d58e68f
sbx_user1051 <sbx_user1051@169.254.25.165>
2023-10-19 21:31:13 +0000

    Thu Oct 19 21:31:13 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -27,14 +27,33 @@ from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    item_counts = Counter(skus)
-    for k,v in item_counts.items():
-        if k=="A":
-            items_reduced_price = int(v / 3)
-            items_regular_price = v % 3
-            a=0
-        print(k,v)
+    prices = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15,
+    }
 
-checkout("AAAAAAABCE")
+    special_offers = {
+        "A": (3,130),
+        "B": (2,45),
+    }
+    item_counts = Counter(skus)
+    total_price = 0
+    for k,v in item_counts.items():
+        if k in special_offers.keys():
+            sets_reduced_price = int(v / special_offers[k][0])
+            items_regular_price = v % special_offers[k][0]
+            total_price += sets_reduced_price * special_offers[k][1]
+            total_price += items_regular_price * prices[k]
+        else:
+            total_price += v * prices[k]
+            a=0
+        print(total_price)
+
+    return total_price
+
+checkout("AAAAAAABBBCCCDDDDDDD")
+
 
 
